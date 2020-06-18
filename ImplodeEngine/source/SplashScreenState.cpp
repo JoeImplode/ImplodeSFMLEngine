304d1309544a2ed4db4f78e32276b491be990963
Joe <joe_pickering5@hotmail.com>
2020-06-18 20:38:13 +0100

    Added the managers filters
    Added managers filters

--- ImplodeEngine/source/SplashScreenState.cpp
+++ ImplodeEngine/source/SplashScreenState.cpp
@@ -10,7 +10,6 @@ SplashScreenState::SplashScreenState(GameState * nextState)
 	m_splashText.setFillColor(sf::Color::Cyan);
 	m_font.loadFromFile("../resources/fonts/Roboto-Light.ttf");
 	m_splashText.setStyle(sf::Text::Bold | sf::Text::Underlined);
-	
 	m_nextState = nextState;
 }
 
